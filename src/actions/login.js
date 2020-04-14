054ffb801f3a1f0c9f6262afae50e742f991ed8a
EduCosta85 <59846713+EduCosta85@users.noreply.github.com>
2020-04-14 16:31:17 -0300

    adicionado componente de edicao de usuario, e mensagem de erro no cadastro

--- src/actions/login.js
+++ src/actions/login.js
@@ -23,20 +23,19 @@ export const doLogin = (user, pass) => async (dispatch) => {
     }
 }
 
-export const doSignUp = (name, email, cpf, password) => async (dispatch) => {
-    try {
-        const response = await axios.post(`${baseUrl}${baseID}/signup`, {
-            name, email, cpf, password
-        })
+export const doSignUp = (name, email, cpf, password) => (dispatch) => {
+    const userSignup = axios.post(`${baseUrl}${baseID}/signup`, {
+        name, email, cpf, password
+    }).then(response => {
         localStorage.setItem("token", response.data.token)
         localStorage.setItem("user", JSON.stringify(response.data.user))
         if (response.data.user.hasAddress === false) {
             dispatch(push(routes.newAddress))
         };
-    } catch (e) {
-        alert("Erro ao logar. Entre em contato conosco")
-        console.error(e)
-    }
+    }).catch(error => {
+        console.log(error.response.data.message)
+        dispatch(setErrorMsg(error.response.data.message))
+    })
 }
 
 export const doNewAddress = (data) => async (dispatch) => {
@@ -53,4 +52,30 @@ export const doNewAddress = (data) => async (dispatch) => {
         alert("Erro ao logar. Entre em contato conosco")
         console.error(e)
     }
+}
+
+export const setErrorMsg = (data) => ({
+    type: "SET_ERROR_MSG",
+    payload: data,
+});
+
+export const doDatarefresh = (name, email, cpf) => (dispatch) => {
+    const token = localStorage.getItem("token")
+    const userDataRefresh = axios.put(`${baseUrl}${baseID}/profile`, {
+        name, email, cpf
+    }, {
+        headers: {
+            auth: token
+        }
+    }).then(response => {
+        localStorage.setItem("user", JSON.stringify(response.data.user))
+        if (response.data.user.hasAddress === false) {
+            dispatch(push(routes.newAddress))
+        } else {
+            dispatch(push(routes.root))
+        }
+    }).catch(error => {
+        console.log(error.response.data.message)
+        dispatch(setErrorMsg(error.response.data.message))
+    })
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import { connectRouter } from "connected-react-router";
 import SaveDetailInState from "./RestaurantDetailReducer";
+import Error from './error'
 
 export const generateReducers = (history) =>
   combineReducers({
     router: connectRouter(history),
     SaveDetailInState,
+    Error
   });
