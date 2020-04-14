eef682d437250216d07be2b0b6c4e26301e10037
eloisa.fagundes <lola.fernanda@hotmail.com>
2020-04-14 19:32:06 -0300

    correção do footer na posição fixed e mock das rotas

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,22 +1,31 @@
 import React, { Component } from 'react'
+import { push } from 'connected-react-router'
+import { connect } from 'react-redux'
 
-import { FooterWrapper, HomeIconStyled, IconsWrapper, ShoppingCartIconStyled, UserIconStyled} from './styles'
+import { FooterWrapper, HomeIconStyled, IconsWrapper, ShoppingCartIconStyled, UserIconStyled } from './styles'
+
 class Footer extends Component {
 
     render() {
         return (
             <FooterWrapper>
-                <IconsWrapper>
+                <IconsWrapper 
+                // onClick={goToHome}
+                >
                 <img src={require("./Images/homepage.png")} type="icon" alt="ícone clicável home"></img>
                     {/* <HomeIconStyled color="primary"></HomeIconStyled> */}
                 </IconsWrapper>
 
-                <IconsWrapper>
+                <IconsWrapper 
+                // onClick={goToShoppingCart}
+                >
                 <img src={require("./Images/shopping-cart.png")} type="icon" alt="ícone clicável carrinho de pedidos"></img>
                 {/* <ShoppingCartIconStyled color="primary"></ShoppingCartIconStyled> */}
                 </IconsWrapper>
 
-                <IconsWrapper>
+                <IconsWrapper 
+                // onClick={goToUserProfile} 
+                >
                 <img src={require("./Images/avatar.png")} type="icon" alt="ícone clicável meu perfil"></img>
                     {/* <UserIconStyled color="primary"></UserIconStyled> */}
                 </IconsWrapper>
@@ -25,4 +34,10 @@ class Footer extends Component {
     }
 }
 
-export default Footer
+const mapDispatchToProps = (dispatch) => ({
+    // goToShoppingCart: () => dispatch (push(routes.shoppingCart)),
+    // goToUserProfile: () => dispatch (push(routes.userProfile)),
+    // goToHome: () => dispatch (push(routes.home))
+})
+
+export default connect(null, mapDispatchToProps)(Footer)
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -3,12 +3,18 @@ import styled from 'styled-components'
 import { } from '@material-ui/core'
 import { HomeOutlined, ShoppingCartOutlined, PersonOutlineOutlined } from '@material-ui/icons'
 
+
 export const FooterWrapper = styled.div`
-width: 360px;
+width: 100%;
 height: 64px;
+-webkit-backdrop-filter: blur(10px);
+backdrop-filter: blur(10px);
+box-shadow: 0 0.5px 0 0 rgba(0, 0, 0, 0.25);
+background-color: #ffffff;
 display: flex;
+align-items: center;
 flex-direction: row;
-position: absolute;
+position: fixed;
 bottom: 0;
 `
 
