ab2d7f3bf5a125c047f77cdd75bd5596342b1218
eloisa.fagundes <lola.fernanda@hotmail.com>
2020-04-13 22:23:43 -0300

    adiciona rota FeedPage/HOME

--- src/containers/Router/index.js
+++ src/containers/Router/index.js
@@ -2,12 +2,17 @@ import React from "react";
 import { ConnectedRouter } from "connected-react-router";
 import { Switch, Route } from "react-router-dom";
 
-const routes = {};
+import FeedPage from '../FeedPage'
+
+const routes = {
+  feed:"/home"
+};
 
 function Router(props) {
   return (
     <ConnectedRouter history={props.history}>
       <Switch>
+        <Route exact path={routes.feed} component={FeedPage} />
         <Route path={routes.root} component={() => <div />} />
       </Switch>
     </ConnectedRouter>
