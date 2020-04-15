b853c72db97127a59ae367e1a5bca06a9d9830b8
eloisa.fagundes <lola.fernanda@hotmail.com>
2020-04-15 14:05:22 -0300

    adiciona arquivo styles.js para os comeponentes feedPage e CartRestaurant

--- src/containers/FeedPage/index.js
+++ src/containers/FeedPage/index.js
@@ -3,38 +3,8 @@ import Footer from "../../components/Footer"
 import CartRestaurant from "../CartRestaurant"
 import { connect } from 'react-redux'
 import { getRestaurants } from "../../actions"
-import styled from "styled-components"
 
-import { TextField } from '@material-ui/core'
-
-const ContentHomeWrapper = styled.div`
-display:flex;
-flex-direction: column;
-justify-content: center;
-align-items:center;
-min-height: 100%;
-position:relative;
-margin-bottom: -50px;
-`
-
-
-// RETIRAR, pois o appbar já está pronta
-const Box = styled.div`
-width: 100%;
-height: 64px;
--webkit-backdrop-filter: blur(10px);
-backdrop-filter: blur(10px);
-box-shadow: 0 0.5px 0 0 rgba(0, 0, 0, 0.25);
-background-color: #ffffff;
-margin-bottom: 2vw;
-  `
-
-const TextFieldStyled = styled(TextField) `
-width: 328px;
-height: 56px;
-border-radius: 2px;
-border: solid 1px #b8b8b8;
-`
+import { ContentHomeWrapper, TextFieldSearchStyled} from './styles'
 
 
 class FeedPage extends Component {
@@ -51,12 +21,12 @@ class FeedPage extends Component {
         const { restaurants } = this.props
         return (
             <>
-                <Box> </Box>
+
 
                 <ContentHomeWrapper>
 
                 <form autoComplete="on">
-                    <TextFieldStyled  label="Restaurante" variant="outlined" />
+                    <TextFieldSearchStyled  label="Restaurante" variant="outlined" />
                 </form>
 
                         {/* FALTA MENU COM SCROLL LATERAL */}
